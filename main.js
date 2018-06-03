7a49d232d0e33a9b3c99f6a8dcdb4846a61e6c1d
Steve <38475652+TetrisCrowley@users.noreply.github.com>
2018-06-03 18:43:46 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+var cardOne = "king";
+var cardTwo = "queen";
+var cardThree = "king";
+var cardFour = "queen";
+console.log("User flipped " + cardOne);
+console.log("User flipped " + cardTwo);
+console.log("User flipped " + cardThree);
+console.log("User flipped " + cardFour);
